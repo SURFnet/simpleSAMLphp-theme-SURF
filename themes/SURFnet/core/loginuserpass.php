b0fda9c477e4b0bd7127a837a0b21f8b805ad3f7
François Kooman <fkooman@tuxed.net>
2012-07-10 11:07:47 +0200

    update simpleSAMLphp theme module to use 1.4 design

--- themes/SURFnet/core/loginuserpass.php
+++ themes/SURFnet/core/loginuserpass.php
@@ -17,9 +17,8 @@ if(array_key_exists('header', $this->data)) {
 ?></title>
 
 	<link rel="stylesheet" type="text/css" href="<?php echo SimpleSAML_Module::getModuleURL('themeSURFnet/style.css'); ?>" />
-	<link rel="stylesheet" media="all and (orientation:landscape)" href="<?php echo SimpleSAML_Module::getModuleURL('themeSURFnet/style_480.css'); ?>" />	
-	<link rel="stylesheet" media="all and (orientation:portrait)" href="<?php echo SimpleSAML_Module::getModuleURL('themeSURFnet/style_320.css'); ?>" />
-	<link rel="stylesheet" media="screen and (min-device-width: 680px)" href="<?php echo SimpleSAML_Module::getModuleURL('themeSURFnet/style_desktop.css'); ?>" />
+  <link rel="stylesheet" media="screen and (max-width: 370px)" href="<?php echo SimpleSAML_Module::getModuleURL('themeSURFnet/style_320.css'); ?>" />
+	<link rel="stylesheet" media="screen and (max-device-width: 480px), handheld" href="<?php echo SimpleSAML_Module::getModuleURL('themeSURFnet/style_480.css'); ?>" />
 	
 	<script type="text/javascript">
 	function initiate(){
@@ -36,7 +35,8 @@ if(array_key_exists('header', $this->data)) {
 		<!-- HEADER MET LOGO, EVENTUELE TITEL EN TAAL TOGGLE -->
 		<div id="header">
 			<img id="logo" src="<?php echo SimpleSAML_Module::getModuleURL('themeSURFnet/surfnet.png'); ?>" /> <!-- DIT IS HET LOGO -->
-			<h1 class="mainTitle"></h1>				  <!-- HIER KAN EEN TITEL -->
+			<h1 class="mainTitle"></h1>				        <!-- HIER KAN EEN TITEL -->
+			<ul class="langSelect">
 
 <?php 
 $includeLanguageBar = TRUE;
@@ -46,13 +46,11 @@ if (isset($this->data['hideLanguageBar']) && $this->data['hideLanguageBar'] === 
 	$includeLanguageBar = FALSE;
 
 if ($includeLanguageBar) {
-	echo '<ul class="langSelect">';
 	$languages = $this->getLanguageList();
 	$langnames = array();
 	foreach($languages as $k => $v) {
 		$langnames[$k] = strtoupper($k);
-	}
-	
+	}	
 	$textarray = array();
 	foreach ($languages AS $lang => $current) {
 		$lang = strtolower($lang);
@@ -64,11 +62,17 @@ if ($includeLanguageBar) {
 		}
 	}
 	echo join($textarray);
-	echo '</ul>';
 }
 ?>
 
-        </div>
+<!--
+				<li class="active"><a href="#">NL</a>
+				<li><a href="#">EN</a>
+				<li><a href="#">DE</a>
+-->
+
+			</ul>
+		</div>
 		<!-- EINDE HEADER MET LOGO, EVENTUELE TITEL EN TAAL TOGGLE -->
 		
 		<div id="content">
@@ -80,22 +84,18 @@ if ($includeLanguageBar) {
 <?php
 if ($this->data['errorcode'] !== NULL) {
 ?>
-<p class="error">
-<!-- <?php echo $this->t('{login:error_header}'); ?> -->
-<!-- <?php echo $this->t('{errors:title_' . $this->data['errorcode'] . '}'); ?> -->
-<?php echo $this->t('{errors:descr_' . $this->data['errorcode'] . '}'); ?>
-</p>
+
+				<p class="error"><?php echo $this->t('{errors:descr_' . $this->data['errorcode'] . '}'); ?></p> <!-- ERRORHANDLER BIJ HET INLOGGEN -->
+
 <?php
 }
-?>
-			
+?>				
 				<form id="login" method="POST" action="?" name="f">
-
-
-					<label for="username"><?php echo $this->t('{login:username}'); ?></label>
-					<input type="text" name="username" id="username" value="<?php echo htmlspecialchars($this->data['username']); ?>" autocompletion= "off" />
+					<label for="username"><?php echo $this->t('{login:username}'); ?></label> <!-- <span class="example">(bv. 123456@catherijne.nl)</span> -->
+					<input type="text" name="username" id="username" autocomplete= "off" />
 					<label for="password"><?php echo $this->t('{login:password}'); ?></label>
-					<input type="password" name="password" id="password" autocompletion= "off" />
+					<input type="password" name="password" id="password" autocomplete= "off" />
+					<!-- <a href="#" class="recover">Wachtwoord vergeten?</a> -->
 					<input type="submit" name="submit" id="submit" value="<?php echo $this->t('{login:login_button}'); ?>" />
 
 <?php
@@ -107,29 +107,30 @@ foreach ($this->data['stateparams'] as $name => $value) {
 				</form>
 				
 			</div>
-			<!-- EINE LOGIN VELD MET BIJBEHOREN -->
+			<!-- EINDE LOGIN VELD MET BIJBEHOREN -->
 			
 			<!-- LIJST MET TIPS -->
 			<div class="subitem">
-			<div class="createIndex">
-				<!-- <ul>
+<!--
+			  <div class="createIndex">
+				 <ul>
 					<h2>Tips:</h2>
-					<li>U inlog blijft behouden voor alle websites en applicaties die ervan gebruikmaken zolang u uw browser niet afsluit. <b>Vergeet uw browser niet af te sluiten om misbruik te voorkomen.</b></li>
-					<li>Controleer altijd voorafgaand aan het inlog de URL van deze pagina, deze moet beginnen met https://federatie.c-college.nl</li>
-					<li>
-						Voor problemen bij het inloggen kun je contact opnemen met de servicedesk, via <a href="https://servicedesk.c-college.nl" target="_blank">https://servicedesk.c-college.nl</a>, telefoon 088-4699070 
-						of email <a href="mailto: servicedesk@c-college.nl">servicedesk@c-college.nl</a>
-					</li>
-				</ul> -->
+					<li><b>Sluiten uw browser af om misbruik te voorkomen.</b> U inlog blijft behouden voor alle websites en applicaties die ervan gebruikmaken zolang u uw browser niet afsluit. </li>
+					<li>Controleer altijd voorafgaand aan het inlog de URL van deze pagina. Deze moet beginnen met <b>https://federatie.c-college.nl</b></li>
+					<li>Heeft uw aanhoudende problemen heeft met inloggen? Neem dan contact opnemen met de servicedesk: email <a href="mailto: servicedesk@c-college.nl">servicedesk@c-college.nl</a>, telefoon 088-4699070</li>
+				</ul>
+			 </div>
 			</div>
-			</div>
+-->
 			<!-- EINDE LIJST MET TIPS -->
 		</div>
 		
 		<!-- FOOTER -->
-<!--		<div id="footer">
-			<a href="#">Help</a>  <a href="#"> Privacy Policy</a>  Copyright © 2012 Catharijne college
-		</div> -->
+<!--		
+        <div id="footer">
+			<p><a href="#">Help</a>  <a href="#"> Privacy Policy</a>  &copy; 2012 Catharijne college</p>
+		</div> 
+-->
 		<!-- EINDE FOOTER -->
 	</div>
 </body>
