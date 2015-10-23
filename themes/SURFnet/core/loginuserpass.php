61c826d07c5fd2069af821bfa5d6268813ab8f32
Thijs Kinkhorst <thijs@kinkhorst.com>
2015-10-23 14:53:53 +0000

    Add alt tags to logos

--- themes/SURFnet/core/loginuserpass.php
+++ themes/SURFnet/core/loginuserpass.php
@@ -47,7 +47,7 @@ if(array_key_exists('header', $this->data)) {
 	
 		<!-- HEADER MET LOGO, EVENTUELE TITEL EN TAAL TOGGLE -->
 		<div id="header">
-			<img id="logo" src="<?php echo SimpleSAML_Module::getModuleURL('themeSURFnet/logo.png'); ?>" /> <!-- DIT IS HET LOGO -->
+			<img id="logo" src="<?php echo SimpleSAML_Module::getModuleURL('themeSURFnet/logo.png'); ?>" alt="" /> <!-- DIT IS HET LOGO -->
 			<h1 class="mainTitle"></h1>				        <!-- HIER KAN EEN TITEL -->
 			<ul class="langSelect">
 
--- themes/SURFnet/default/error.php
+++ themes/SURFnet/default/error.php
@@ -42,7 +42,7 @@ if(array_key_exists('header', $this->data)) {
 	
 		<!-- HEADER MET LOGO, EVENTUELE TITEL EN TAAL TOGGLE -->
 		<div id="header">
-			<img id="logo" src="<?php echo SimpleSAML_Module::getModuleURL('themeSURFnet/logo.png'); ?>" />
+			<img id="logo" src="<?php echo SimpleSAML_Module::getModuleURL('themeSURFnet/logo.png'); ?>" alt="" />
 			<h1 class="mainTitle"></h1>
 			<ul class="langSelect">
 
